81793167de4697e541055c8c78fc2fd912a725de
TSVTLN <tsvtln@abv.bg>
2023-11-19 23:32:42 +0200

    working song queue

--- LoFi_bot/LoFi_bot.py
+++ LoFi_bot/LoFi_bot.py
@@ -1,28 +1,24 @@
 """
-this works and can play music, needs good upload speed of at least 25
+this works and can play music, needs good upload speed of at least 25Mbps
+
 to do:
-- implement queue's
 - implement buttons
-- implement interface to be displayed in discord
+
 done:
 - implemented search function
+- implemented queue's
+- implemented text interface to be displayed in discord of what is currently playing
 """
 
-import discord, asyncio, yt_dlp
-# import pafy, urllib.request, json, urllib, os
-# from discord.ext import commands
-# from discord_buttons_plugin import *
-# from pytube import Playlist
+import discord
+import asyncio
+import yt_dlp
 from collections import deque
 
 with open('bot_keys', 'r') as f:
     bot_token = f.read().strip()
 
-# lofi_bot = commands.Bot(command_prefix='$', intents=discord.Intents.all())
-# lofi_bot.add_cog(help_cog(lofi_bot))
-
 client = discord.Client(command_prefix='$', intents=discord.Intents.all())
-# bot = commands.Bot(command_prefix='$', intents=discord.Intents.all())
 key = bot_token
 voice_clients = {}
 yt_dl_opts = {"format": 'bestaudio/best'}
@@ -31,27 +27,57 @@ song_queues = {}
 song_queue_name = deque()
 voice_status = 'not connected'
 url = ''
+bot_chat = None
 
 ffmpeg_options = {'options': '-vn -reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 15'}
 
 
-# runtime configuration
+async def play_next_song(guild_id, msg):
+    global bot_chat
+    if guild_id in song_queues and song_queues[guild_id]:
+        next_url = song_queues[guild_id][0]
+        if 'MEGALOVANIA' in get_video_name(next_url):
+            bot_chat = 'https://tenor.com/view/funny-dance-undertale-sans-gif-26048955'
+        elif "I'VE GOT NO FRIENDS" in get_video_name(next_url):
+            bot_chat = 'https://tenor.com/view/actorindie-worlds-smallest-violin-aww-violin-gif-13297153'
+        elif 'hipodil' in get_video_name(next_url).lower():
+            bot_chat = 'https://tenor.com/view/cat-music-listening-gif-18335467'
+
+        if bot_chat:
+            await msg.channel.send(bot_chat)
+        else:
+            await msg.channel.send(get_video_name(next_url))
+        song_queues[guild_id].pop(0)
+        song_queue_name.popleft()
+        next_song = await asyncio.to_thread(ytdl.extract_info, next_url, {'download': True})
+        next_audio = discord.FFmpegPCMAudio(next_song['url'], **ffmpeg_options, executable="/usr/bin/ffmpeg")
+        voice_clients[guild_id].play(next_audio,
+                                     after=lambda e: asyncio.run_coroutine_threadsafe(play_next_song(guild_id, msg),
+                                                                                      client.loop))
+        await client.change_presence(activity=discord.Game(name=get_video_name(next_url)))
+
+
+@client.event
+async def on_voice_state_update(member, before, after):
+    if after.channel is None and voice_clients.get(member.guild.id) is not None:
+        if not voice_clients[member.guild.id].is_playing() and song_queues.get(member.guild.id):
+            await play_next_song(member.guild.id, None)
+
+
 @client.event
 async def on_ready():
     print(f"Bot is ready")
     await client.change_presence(activity=discord.Game(name='цигу мигу чакарака'))
 
 
-# Receiver
 @client.event
 async def on_message(msg):
     global voice_status, url
     if msg.content.startswith('$play'):
-
-        # create voice client connection
         if voice_status != 'connected':
             try:
-                voice_client = await msg.author.voice.channel.connect()
+                voice_channel = msg.author.voice.channel
+                voice_client = await voice_channel.connect()
                 voice_clients[voice_client.guild.id] = voice_client
                 voice_status = 'connected'
             except Exception as err:
@@ -59,9 +85,7 @@ async def on_message(msg):
                 voice_status = 'not connected'
                 print(err)
 
-        # play song
         try:
-            # test if there's more than 1 word to use in search query
             test_for_url = msg.content.split()
             if len(test_for_url) > 1:
                 test_for_url = deque(test_for_url[1:])
@@ -69,24 +93,18 @@ async def on_message(msg):
             else:
                 url = test_for_url[1]
 
-            bot_chat = None
             if url == 'skakauec':
                 url = 'https://www.youtube.com/watch?v=pq3C-UE6RE0'
-                bot_chat = 'Пущам СКАКАУЕЦ!'
             elif url == 'sans':
                 url = 'https://www.youtube.com/watch?v=0FCvzsVlXpQ'
-                bot_chat = 'https://tenor.com/view/funny-dance-undertale-sans-gif-26048955'
             elif url == 'ignf':
                 url = 'https://www.youtube.com/watch?v=yLnd3AYEd2k'
-                bot_chat = 'https://tenor.com/view/actorindie-worlds-smallest-violin-aww-violin-gif-13297153'
             elif 'http' not in url:
                 url = find_video_url(url)
 
-            # checking if guild has a queue
             if msg.guild.id not in song_queues:
                 song_queues[msg.guild.id] = []
 
-            # notify in chat that the song is added
             if get_video_name(url) != 'Video title not available' and \
                     get_video_name(url) != 'Error retrieving video title':
                 await msg.channel.send(f"Добавена песен в плейлиста: {get_video_name(url)}")
@@ -95,21 +113,8 @@ async def on_message(msg):
             else:
                 await msg.channel.send('Пробуем при намирането на таз песен.')
 
-            # if only 1 song, play it
-            if len(song_queues[msg.guild.id]) >= 1:
-                loop = asyncio.get_event_loop()
-                data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=True))
-
-                song = data['url']
-                player = discord.FFmpegPCMAudio(song, **ffmpeg_options, executable="/usr/bin/ffmpeg")
-
-                # voice_client.play(player)
-                voice_clients[msg.guild.id].play(player)
-
-                if bot_chat:
-                    await msg.channel.send(bot_chat)
-
-                await client.change_presence(activity=discord.Game(name=get_video_name(url)))
+            if len(song_queues[msg.guild.id]) == 1 and not voice_clients[msg.guild.id].is_playing():
+                await play_next_song(msg.guild.id, msg)
 
         except yt_dlp.DownloadError:
             await msg.channel.send(f"Нема такова '{str(url)}'")
@@ -145,9 +150,7 @@ async def on_message(msg):
 
     if msg.content.startswith("$queue"):
         if msg.guild.id in song_queues and song_queue_name:
-            formatted_queue = []
-            for number_of_song_in_queue, song_name_in_queue in enumerate(song_queue_name, 1):
-                formatted_queue.append(f"{number_of_song_in_queue}. {song_name_in_queue}")
+            formatted_queue = [f"{i}. {name}" for i, name in enumerate(song_queue_name, 1)]
             queue_list = '\n'.join(formatted_queue)
             await msg.channel.send(f"Плейлист:\n{queue_list}")
         else:
@@ -168,14 +171,9 @@ def get_video_name(youtube_url):
 
 
 def find_video_url(search_query):
-    # Create a YDL object with some options
     ydl_opts = yt_dl_opts
-
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        # Search for the video by name
         video = ydl.extract_info(f"ytsearch:{search_query}", ie_key='YoutubeSearch')['entries'][0]
-
-        # Extract the video URL
         return video['webpage_url']
 
 
